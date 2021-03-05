806abac750cf0d627ae585be8765e4823330f01d
Amon <amonkmli@gmail.com>
2021-03-06 00:20:48 +0700

    Version 1 first try to save

---
+++ Lab1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.io.*;
+public class Lab1 {//class
+    public static void main(String[] args) throws IOException {//main
+        System.out.println("===================");
+        System.out.println("Hello World");
+        System.out.println("===================");
+    
+    /* Comment 
+        with 
+        Multiple 
+        Line
+    */
+    int a=12;//int
+    double b=23.32;//double
+    boolean myBool = true;//boolean
+    String myText = "Hello";//string
+    float myFloatNum = 5.99f;//Float
+        System.out.println("This is a String " + a);
+        System.out.println("This is a String " + b);
+        System.out.println("This is a String " + myBool);
+        System.out.println("This is a String " + myText);
+        System.out.println("This is a String " + myFloatNum);
+    }//main
+}
